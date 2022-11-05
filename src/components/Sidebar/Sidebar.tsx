75077557cf9b95d00ff352dafee2945c6c91973a
kangju2000 <kangju2000@naver.com>
2022-11-06 02:25:45 +0900

    refactor: Sidebar 컴포넌트에서 styled 방식을 css 방식으로 변경

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,25 +1,26 @@
-import styled from '@emotion/styled';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 
 interface sidebarProps {
   isOpen: boolean;
 }
 
-const StyledSideBar = styled.nav`
-  position: absolute;
-  left: ${({ isOpen }: sidebarProps) => (isOpen ? '0px' : '-100px')};
-  width: 100px;
-  height: 100vh;
-  background-color: red;
-  transition: all 0.5s;
-`;
-
 const Sidebar = ({ isOpen }: sidebarProps) => {
   return (
-    <StyledSideBar isOpen={isOpen}>
+    <nav
+      css={css`
+        position: absolute;
+        left: ${isOpen ? '0px' : '-100px'};
+        width: 100px;
+        height: 100vh;
+        background-color: red;
+        transition: all 0.5s;
+      `}
+    >
       <p>Top 100</p>
       <p>New Released</p>
       <p></p>
-    </StyledSideBar>
+    </nav>
   );
 };
 
