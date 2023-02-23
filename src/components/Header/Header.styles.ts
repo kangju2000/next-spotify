6e639accf2e58e21e57d616cd3459ef46a91ea48
kangju2000 <kangju2000@naver.com>
2023-02-24 02:04:30 +0900

    style: Header 컴포넌트 UI 구현

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -1,8 +1,27 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.header`
-  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 50%;
+  min-width: 600px;
   height: 100px;
 `;
 
-export const Clock = styled.div``;
+export const Clock = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-left: 20px;
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+export const Time = styled.p`
+  margin-bottom: 15px;
+  font-size: 24px;
+  font-weight: 700;
+`;
+
+export const Date = styled.p`
+  font-size: 12px;
+`;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,6 +4,10 @@ import * as S from './Header.styles';
 const Header = () => {
   return (
     <S.Container>
+      <S.Clock>
+        <S.Time>19:00</S.Time>
+        <S.Date>2022.02.22 수요일</S.Date>
+      </S.Clock>
       <SearchInput placeholder="듣고 싶은 음악을 검색해보세요." />
     </S.Container>
   );
