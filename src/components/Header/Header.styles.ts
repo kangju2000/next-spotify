c45e535152f90279b49cd8986dbaea401e9fe269
kangju2000 <kangju2000@naver.com>
2023-02-23 17:57:20 +0900

    style: Header 스타일 변경

---
+++ src/components/Header/Header.styles.ts
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.header`
+  width: 100%;
+  height: 100px;
+`;
+
+export const Clock = styled.div``;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,56 +1,11 @@
-/** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react';
-import Sidebar from 'components/Sidebar/Sidebar';
-import Image from 'next/image';
-import { useRouter } from 'next/router';
-import { useState } from 'react';
+import SearchInput from 'components/SearchInput/SearchInput';
+import * as S from './Header.styles';
 
 const Header = () => {
-  const router = useRouter();
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-
-  const onMenuClick = () => {
-    setIsOpen((prev) => !prev);
-  };
-
   return (
-    <>
-      <Sidebar isOpen={isOpen} />
-      <header
-        css={css`
-          position: relative;
-          padding: 48px;
-          text-align: center;
-        `}
-      >
-        <span
-          css={css`
-            position: absolute;
-            left: 48px;
-            cursor: pointer;
-            transition: transform 1s;
-            &:focus {
-              transform: rotate(90deg);
-            }
-          `}
-        >
-          <Image alt="메뉴" src="/images/menu.svg" width="24" height="24" onClick={onMenuClick} />
-        </span>
-        <p
-          css={css`
-            font-size: 24px;
-            font-weight: bold;
-            cursor: pointer;
-            &:focus {
-              transform: rotate(90deg);
-            }
-          `}
-          onClick={() => router.push('/')}
-        >
-          Search Music
-        </p>
-      </header>
-    </>
+    <S.Container>
+      <SearchInput placeholder="듣고 싶은 음악을 검색해보세요." />
+    </S.Container>
   );
 };
 
