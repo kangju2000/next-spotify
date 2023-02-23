a8b3cae8f9bf3d666243f04c1a0515d55dd1c16d
kangju2000 <kangju2000@naver.com>
2023-02-23 17:56:48 +0900

    style: Sidebar 스타일 변경

---
+++ src/components/Sidebar/Sidebar.styles.ts
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.nav`
+  width: 100px;
+  height: 100vh;
+  background: ${({ theme }) => theme.colors.black};
+`;
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,27 +1,7 @@
-/** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react';
+import * as S from './Sidebar.styles';
 
-interface sidebarProps {
-  isOpen: boolean;
-}
-
-const Sidebar = ({ isOpen }: sidebarProps) => {
-  return (
-    <nav
-      css={css`
-        position: absolute;
-        left: ${isOpen ? '0px' : '-100px'};
-        width: 100px;
-        height: 100vh;
-        background-color: red;
-        transition: all 0.5s;
-      `}
-    >
-      <p>Top 100</p>
-      <p>New Released</p>
-      <p></p>
-    </nav>
-  );
+const Sidebar = () => {
+  return <S.Container></S.Container>;
 };
 
 export default Sidebar;
